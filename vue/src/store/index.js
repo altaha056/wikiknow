fbd58503d913bb21fc7361d923f10888b1ee7e56
Altaha <altahabinsyafriadi@gmail.com>
2022-03-23 16:24:09 +0700

    create store

---
+++ vue/src/store/index.js
@@ -0,0 +1,17 @@
+import {createStore} from "vuex"
+const store = createStore({
+  state: {
+      user:{
+          data:{
+              name:'altaha',
+              token:null
+          }
+      }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {},
+});
+
+export default store
