90981be416807b120e0d1de0ab961d06917b81eb
Altaha <altahabinsyafriadi@gmail.com>
2022-04-01 22:15:15 +0700

    remove bell icon and create log out button

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -40,13 +40,8 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button
-                type="button"
-                class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              >
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="h-6 w-6" aria-hidden="true" />
-              </button>
+              
+              
 
               <!-- Profile dropdown -->
               <Menu as="div" class="ml-3 relative">
@@ -74,17 +69,14 @@
                     class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                   >
                     <MenuItem
-                      v-for="item in userNavigation"
-                      :key="item.name"
                       v-slot="{ active }"
                     >
                       <a
-                        :href="item.href"
+                        @click="logout"
                         :class="[
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700',
+                          'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
                         ]"
-                        >{{ item.name }}</a
+                        >Log out</a
                       >
                     </MenuItem>
                   </MenuItems>
@@ -134,22 +126,14 @@
                 {{ user.email }}
               </div>
             </div>
-            <button
-              type="button"
-              class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-            >
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
+            
           </div>
           <div class="mt-3 px-2 space-y-1">
             <DisclosureButton
-              v-for="item in userNavigation"
-              :key="item.name"
               as="a"
-              :href="item.href"
-              class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-              >{{ item.name }}</DisclosureButton
+              @click="logout"
+              class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"
+              >Log out</DisclosureButton
             >
           </div>
         </div>
@@ -178,11 +162,6 @@ const navigation = [
   { name: "Dashboard", to: { name: "Dashboard" } },
   { name: "Surveys", to: { name: "Surveys" } },
 ];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
 
 export default {
   components: {
@@ -202,7 +181,6 @@ export default {
     return {
       user: computed(() => store.state.user.data),
       navigation,
-      userNavigation,
     };
   },
 };
