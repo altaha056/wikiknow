a2b1b029814ed5a90ad0dd7f99959bd3c6302f79
Altaha <altahabinsyafriadi@gmail.com>
2022-04-01 12:13:44 +0700

    update header on website

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,6 +4,8 @@ import Surveys from "../views/Surveys.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import DefaultLayout from "../components/DefaultLayout.vue";
+import AuthLayout from "../components/AuthLayout.vue";
+import store from "../store";
 
 const routes = [
   {
@@ -11,6 +13,7 @@ const routes = [
     redirect: "/dashboard",
     name: "Dashboard",
     component: DefaultLayout,
+    meta: { requiresAuth: true },
     children: [
       {
         path: "/dashboard",
@@ -25,20 +28,39 @@ const routes = [
     ],
   },
   {
-    path: "/login",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register,
+    path: "/auth",
+    redirect: "/login",
+    name: "Auth",
+    component: AuthLayout,
+    meta: { isGuest: true },
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: Login,
+      },
+      {
+        path: "/register",
+        name: "Register",
+        component: Register,
+      },
+    ],
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: "Login" });
+  } else if (store.state.user.token && to.meta.isGuest) {
+    next({ name: "Dashboard" });
+  } else {
+    next();
+  }
 });
 
 export default router;
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,9 +3,12 @@ const store = createStore({
   state: {
     user: {
       data: {
-        name: "altaha",
-        token: null,
+        name: "Altaha",
+        email: "altaha@alta.com",
+        imageUrl:
+          "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
       },
+      token: 23423,
     },
   },
   getters: {},
